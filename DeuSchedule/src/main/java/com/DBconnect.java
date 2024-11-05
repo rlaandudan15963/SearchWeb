56353be8f4229420141ff59f2a4e9ba0801d289a
kmlim <kmlim@kmsubcom>
2024-11-05 20:21:57 +0900

    회원가입 넘어가는 부분 일부 수정 + 임시 메인화면

--- DeuSchedule/src/main/java/com/DBconnect.java
+++ DeuSchedule/src/main/java/com/DBconnect.java
@@ -5,10 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBconnect {
+	//DB 접속 함수
 	public static Connection getConnection() throws SQLException {
         Connection conn = null;
         try {
-            String url = "jdbc:oracle:thin:@localhost:1521:xe";//오라클 주소(딱히 건들진 말것)
+            String url = "jdbc:oracle:thin:@localhost:1521:xe";//오라클 주소(localhost를 DB실행한 컴 ip로 설정)
             String user = "DEUWEB";//연결할 DB이름
             String password = "1234";//비번
 
